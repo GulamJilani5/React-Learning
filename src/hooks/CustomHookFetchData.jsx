86cd2abe71dfb024938605e79e80183a0e6ab34b
Jilani <gulam11jilani12@gmail.com>
2024-07-29 14:40:58 +0530

    parentComponent rerendering logic with child compoennt

--- src/hooks/CustomHookFetchData.jsx
+++ src/hooks/CustomHookFetchData.jsx
@@ -21,7 +21,7 @@
 //                 // console.log('b')
 //                 setData(data);
 //                 setLoading(false)
-    
+
 //             } catch (error) {
 //                 console.log(error)
 //                 setData();
@@ -34,13 +34,12 @@
 //  return {data, loading}
 // }
 
-
 // function CustomHookFetchData (){
 //   const url = "https://jsonplaceholder.typicode.com/users/1";
 //   const {data, loading} = useFetch(url)
 // //   console.log('laoding ',data)
 // //   console.log('Data ',data)
-  
+
 //   // ***Guard Clause
 //   if(data === undefined) return
 
@@ -50,9 +49,9 @@
 
 //      // ***Guard Clause
 //   if(data === undefined) return
-    
+
 //     const{name, email, phone} = data;
-    
+
 //     // Nested Destructuring
 //     const { address: {street, city, zipcode} } = data;
 //     return(
@@ -67,133 +66,83 @@
 //            {email}
 //         </p>
 //         <p>
-//           <b>Phone Number: </b> 
+//           <b>Phone Number: </b>
 //           {phone}
 //         </p>
-//         <b>Address: </b> 
+//         <b>Address: </b>
 //            {street} {city}, {zipcode}
 //         </div>
 //     )
 // }
 // export default CustomHookFetchData;
 
-
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////// METHOD 2 - personal way 
-////////////////// *** REDNERING ISSUE IS THERE. I HAVE TO LEARN, UNDERSTAND AND SOLVE - NOW RESOLVED USING GAURD CLAUSE
-///////////////// - SOLUTION FOR RENDERING IS GUARD CLAUSE TO CHECK IF DATA IS UNDEFIND THEN RETURN NOTHING
+///////////////////////////////////////////////////////////////// METHOD 2 - personal way
+//////////// *** REDNERING ISSUE IS THERE. I HAVE TO LEARN, UNDERSTAND AND SOLVE - NOW RESOLVED USING GAURD CLAUSE
+//////////// - SOLUTION FOR RENDERING IS GUARD CLAUSE TO CHECK IF DATA IS UNDEFIND THEN RETURN NOTHING
+//////// We can put the 'useFetch' and 'CustomHookFetchData' into separate files for better file structure.
 import { useEffect, useState } from "react";
 
-const  useFetch = (url) => {
-
-     const[data, setData] = useState();
-     const[loading, setLoading] = useState(false)
-    //  console.log('inside useFetch custom hook')
-    const fetchData = async (url) => {
-        try {
-            setLoading(true)
-            // console.log('inside fetchData function')
-            const response = await fetch(url)
-            // console.log('a')
-            // console.log(response)
-            const data = await response.json();
-            // console.log(data)
-            // console.log('b')
-            setData(data);
-            setLoading(false)
-        } catch (error) {
-            console.log(error)
-            setData();
-            setLoading(false)
-        }
+const useFetch = (url) => {
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(false);
+  const fetchData = async (url) => {
+    try {
+      setLoading(true);
+      const response = await fetch(url);
+      const data = await response.json();
+      setData(data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setData();
+      setLoading(false);
     }
-    
-    // useEffect((url)=>{   // ***THERE SHOULD BE NO PARAMETER OTHERWISE IT WILL THROW ERROR 
-    useEffect(()=>{
-       fetchData(url)
-       // fetchData("https://jsonplaceholder.typicode.com/users/1")
-    }, [])
+  };
+  // useEffect((url)=>{   // ***THERE SHOULD BE NO PARAMETER OTHERWISE IT WILL THROW ERROR
+  useEffect(() => {
+    fetchData(url);
+    // fetchData("https://jsonplaceholder.typicode.com/users/1")
+  }, []);
 
- return {data, loading}
-}
+  return { data, loading };
+};
 
-function CustomHookFetchData (){
+function CustomHookFetchData() {
   const url = "https://jsonplaceholder.typicode.com/users/1";
-  const {data, loading} = useFetch(url)
-    // console.log(data)
-    // console.log(loading)
-      if (loading) {
-      return <p>Loading...</p>;
-    }
+  const { data, loading } = useFetch(url);
+  // console.log(data)
+  // console.log(loading)
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
-     // ***Guard Clause
-   if(data === undefined) return
+  // ***Guard Clause
+  if (data === undefined) return;
 
-    const{name, email, phone} = data;
-    // Nested Destructuring
-    const { address: {street, city, zipcode} } = data;
-    return(
-        <div>
-          <h1> Custom Hook for fetching data from API</h1>
-          <p>
-            <b>Name: </b>
-            {name}
-          </p>
-          <p>
-            <b>Email :</b>
-            {email}
-          </p>
-          <p>
-            <b>Phone Number:</b> 
-            {phone}
-          </p>
-          <b>Address: </b> 
-            {street} {city}, {zipcode}
-
-        </div>
-    )
+  const { name, email, phone } = data;
+  // Nested Destructuring
+  const {
+    address: { street, city, zipcode },
+  } = data;
+  return (
+    <div>
+      <h1> Custom Hook for fetching data from API</h1>
+      <p>
+        <b>Name: </b>
+        {name}
+      </p>
+      <p>
+        <b>Email :</b>
+        {email}
+      </p>
+      <p>
+        <b>Phone Number:</b>
+        {phone}
+      </p>
+      <b>Address: </b>
+      {street} {city}, {zipcode}
+    </div>
+  );
 }
 export default CustomHookFetchData;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/hooks/UseContext.jsx
+++ src/hooks/UseContext.jsx
@@ -1,35 +1,34 @@
-import { createContext, useContext } from "react"
+import { createContext, useContext } from "react";
 
 const SomeContext = createContext();
 
-export default function UseContext(){
-    return(
-           <SomeContext.Provider value ='SomeValue'>
-                <A/>      
-           </SomeContext.Provider>
-          )
-     }
-
- function A() {
-//    const contextValue = useContext(SomeContext)
-//    console.log('Inside Nested Component A: ', contextValue);
-    return(
-          <div>
-            <h5>Nested Component A</h5>
-            {/* <p>{contextValue}</p> */}
-            <B/>
-          </div>
-         )
+export default function UseContext() {
+  return (
+    <SomeContext.Provider value="SomeValue">
+      <A />
+    </SomeContext.Provider>
+  );
 }
 
- function B() {
-   const contextValue = useContext(SomeContext);
-   console.log('Inside Nested Component B: ', contextValue);
-    return(
-          <div>
-            <h5>Nested Component B</h5>
-            <p>{contextValue}</p>
-          </div>
-         )
+function A() {
+  //    const contextValue = useContext(SomeContext)
+  //    console.log('Inside Nested Component A: ', contextValue);
+  return (
+    <div>
+      <h5>Nested Component A</h5>
+      {/* <p>{contextValue}</p> */}
+      <B />
+    </div>
+  );
 }
 
+function B() {
+  const contextValue = useContext(SomeContext);
+  console.log("Inside Nested Component B: ", contextValue);
+  return (
+    <div>
+      <h5>Nested Component B</h5>
+      <p>{contextValue}</p>
+    </div>
+  );
+}
